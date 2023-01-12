7ca5c90187976e40ca90b7819b0d06b0e0f62efa
Kimberly Nolsin <kimberly.nolsin@gmail.com>
2023-01-11 19:40:30 -0500

    Importing axios library

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,28 +1,41 @@
 import "./HomePage.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import data from "../../data/video-details.json";
 import shortData from "../../data/videos.json";
 import NextVideo from "../../components/NextVideo/NextVideo";
 import Comments from "../../components/Comments/Comments";
 import VideoPlayerDiv from "../../components/VideoPlayerDiv/VideoPlayerDiv";
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
+import axios from "axios";
 
 function HomePage() {
-  const [vidDetails, setDefaultVid] = useState(data);
-  const [currentVid, setCurrentVid] = useState(vidDetails[0]);
-  const [nextVideos, setNextVideos] = useState(shortData);
+  const URL =
+    "https://project-2-api.herokuapp.com/videos?api_key=b694f26b-0dfd-4f61-bd97-068859087909";
+
+  // const [vidDetails, setDefaultVid] = useState(data);
+  // const [currentVid, setCurrentVid] = useState(vidDetails[0]);
+  // const [nextVideos, setNextVideos] = useState(shortData);
+  const [comments, getComments] = useState(null);
+  
 
   function handlerFunc(e) {
-    const newCurrentVid = vidDetails.find(
-      (vid) => vid.id === e.currentTarget.id
-    );
-    setCurrentVid(newCurrentVid);
+  //   const newCurrentVid = vidDetails.find(
+  //     (vid) => vid.id === e.currentTarget.id
+  //   );
+  //   setCurrentVid(newCurrentVid);
 
-    const newArr = shortData.filter(
-      (element) => element.id !== e.currentTarget.id
-    );
-    setNextVideos(newArr);
-  }
+  //   const newArr = shortData.filter(
+  //     (element) => element.id !== e.currentTarget.id
+  //   );
+  //   setNextVideos(newArr);
+  // }
+
+  useEffect(() => {
+    const getComments = async () => {
+      const { data } = await axios.get(URL)
+    } 
+    getComments(data)
+  },[]);
 
   return (
     <>
